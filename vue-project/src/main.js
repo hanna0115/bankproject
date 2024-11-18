c721637a5f875b8691349fab40ad17fcc481c826
keemsnmyn <keemsnmyn@naver.com>
2024-11-18 16:19:42 +0900

    포스트 디테일 수정 필요, 지도 페이지 구현 중(상세 페이지)

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { useKakao } from 'vue3-kakao-maps';
+
+useKakao('b58d7d3c1cb707eebc5096f6365a6285', ['clusterer', 'services', 'drawing'])
 
 const app = createApp(App)
 
