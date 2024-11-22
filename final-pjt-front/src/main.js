9c04588c01973de9d76c9b13dcdc9b1c92f21190
yunha <dbsgk45@naver.com>
2024-11-22 12:50:29 +0900

    프로필페이지에 차트 이미지 추가 끝끝끝

--- final-pjt-front/src/main.js
+++ final-pjt-front/src/main.js
@@ -4,6 +4,7 @@ import 'primeicons/primeicons.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import VueApexCharts from 'vue3-apexcharts'
 
 import App from './App.vue'
 import router from './router'
@@ -15,6 +16,7 @@ const app = createApp(App)
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
+app.use(VueApexCharts);
 app.use(pinia)
 app.use(router)
 
